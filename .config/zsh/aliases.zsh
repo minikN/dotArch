8f75ffe0d7de44e716f4ae6fe05b9a6aa4063c7c
Demis Balbach <demisbalbach@googlemail.com>
2020-09-08 16:45:37 +0200

    Add ssh and qutebrowser aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -13,3 +13,10 @@ alias lss="ls --group-directories-first --color=always -laAhU"
 function gc {
 	git clone "$1" "$GITDIR/$(basename $1 .git)"
 }
+
+# ssh
+alias elftower="ssh -p 22200 demis@116.203.110.209"
+alias mailserver="ssh -p 22 root@195.201.20.120"
+
+# qutebrowser
+alias qutebrowser="qutebrowser -R"
