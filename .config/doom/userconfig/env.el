822f9ad1973f59664f4b33eee068693f95831180
Demis Balbach <db@minikn.xyz>
2020-10-04 15:41:14 +0200

    No linux on surface for now

--- .config/doom/userconfig/env.el
+++ .config/doom/userconfig/env.el
@@ -4,8 +4,3 @@
 (if (string-match "Microsoft" (car (split-string (shell-command-to-string "uname -r"))))
     (setq ENV "wsl")
   (setq ENV "linux"))
-
-; Determine which device we are on.
-(if (equal (x-display-pixel-height) 1440)
-    (setq ENV_WSL "desktop")
-  (setq ENV_WSL "surface"))
