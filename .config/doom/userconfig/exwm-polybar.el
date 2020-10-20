df314f9a8e1e81015f9269f980b4f7cd74801ce8
Demis Balbach <db@minikn.xyz>
2020-10-20 12:43:03 +0200

    Fix error in CHAT not being yellow; simplify function

--- .config/doom/userconfig/exwm-polybar.el
+++ .config/doom/userconfig/exwm-polybar.el
@@ -5,28 +5,18 @@
   "Start polybar."
   (start-process-shell-command "polybar-msg" nil "polybar-msg cmd quit")
   (interactive)
-  (if (equal ENV "surface")
-      (start-process-shell-command "polybar" "*polybar*" "polybar -c=/home/demis/.config/polybar/bar-single.ini single")
-    (start-process-shell-command "polybar" "*polybar*" "polybar -c=/home/demis/.config/polybar/bar-single.ini single")))
+  (start-process-shell-command "polybar" "*polybar*" "polybar -c=/home/demis/.config/polybar/bar-single.ini single"))
 
 ;;(db/start-polybar)
 (advice-add 'enable-theme :after #'db/start-polybar)
 
-(if (equal ENV "surface")
-    (setq WORKSPACE_1 ""
-          WORKSPACE_2 ""
-          WORKSPACE_3 ""
-          WORKSPACE_4 ""
-          WORKSPACE_5 ""
-          WORKSPACE_6 ""
-          WORKSPACE_7 "")
-    (setq WORKSPACE_1 ""
-          WORKSPACE_2 ""
-          WORKSPACE_3 ""
-          WORKSPACE_4 ""
-          WORKSPACE_5 ""
-          WORKSPACE_6 ""
-          WORKSPACE_7 ""))
+(setq WORKSPACE_1 ""
+      WORKSPACE_2 ""
+      WORKSPACE_3 ""
+      WORKSPACE_4 ""
+      WORKSPACE_5 ""
+      WORKSPACE_6 ""
+      WORKSPACE_7 "")
 
 ;; Setting workspaces for polybar
 (defun dw/polybar-exwm-workspace ()
@@ -36,7 +26,7 @@
     (2 (concat "%{F" THEME_B6 "}" WORKSPACE_1 "%{F-} WWW   %{F" THEME_B6 "}" WORKSPACE_2 "%{F-} TERM   %{F" THEME_YELLOW "}" WORKSPACE_3 " CODE%{F-}   %{F" THEME_B6 "}" WORKSPACE_4 "%{F-} AGENDA   %{F" THEME_B6 "}" WORKSPACE_5 "%{F-} MUSIC   %{F" THEME_B6 "}" WORKSPACE_6 "%{F-} CHAT   %{F" THEME_B6 "}" WORKSPACE_7 "%{F-} GAMES"))
     (3 (concat "%{F" THEME_B6 "}" WORKSPACE_1 "%{F-} WWW   %{F" THEME_B6 "}" WORKSPACE_2 "%{F-} TERM   %{F" THEME_B6 "}" WORKSPACE_3 "%{F-} CODE   %{F" THEME_YELLOW "}" WORKSPACE_4 " AGENDA%{F-}   %{F" THEME_B6 "}" WORKSPACE_5 "%{F-} MUSIC   %{F" THEME_B6 "}" WORKSPACE_6 "%{F-} CHAT   %{F" THEME_B6 "}" WORKSPACE_7 "%{F-} GAMES"))
     (4 (concat "%{F" THEME_B6 "}" WORKSPACE_1 "%{F-} WWW   %{F" THEME_B6 "}" WORKSPACE_2 "%{F-} TERM   %{F" THEME_B6 "}" WORKSPACE_3 "%{F-} CODE   %{F" THEME_B6 "}" WORKSPACE_4 "%{F-} AGENDA   %{F" THEME_YELLOW "}" WORKSPACE_5 " MUSIC%{F-}   %{F" THEME_B6 "}" WORKSPACE_6 "%{F-} CHAT   %{F" THEME_B6 "}" WORKSPACE_7 "%{F-} GAMES"))
-    (5 (concat "%{F" THEME_B6 "}" WORKSPACE_1 "%{F-} WWW   %{F" THEME_B6 "}" WORKSPACE_2 "%{F-} TERM   %{F" THEME_B6 "}" WORKSPACE_3 "%{F-} CODE   %{F" THEME_B6 "}" WORKSPACE_4 "%{F-} AGENDA   %{F" THEME_B6 "}" WORKSPACE_5 "%{F-} MUSIC   %{F" THEME_YELLOW "}" WORKSPACE_6 "%{F-} CHAT   %{F" THEME_B6 "}" WORKSPACE_7 "%{F-} GAMES"))
+    (5 (concat "%{F" THEME_B6 "}" WORKSPACE_1 "%{F-} WWW   %{F" THEME_B6 "}" WORKSPACE_2 "%{F-} TERM   %{F" THEME_B6 "}" WORKSPACE_3 "%{F-} CODE   %{F" THEME_B6 "}" WORKSPACE_4 "%{F-} AGENDA   %{F" THEME_B6 "}" WORKSPACE_5 "%{F-} MUSIC   %{F" THEME_YELLOW "}" WORKSPACE_6 " CHAT%{F-}   %{F" THEME_B6 "}" WORKSPACE_7 "%{F-} GAMES"))
     (6 (concat "%{F" THEME_B6 "}" WORKSPACE_1 "%{F-} WWW   %{F" THEME_B6 "}" WORKSPACE_2 "%{F-} TERM   %{F" THEME_B6 "}" WORKSPACE_3 "%{F-} CODE   %{F" THEME_B6 "}" WORKSPACE_4 "%{F-} AGENDA   %{F" THEME_B6 "}" WORKSPACE_5 "%{F-} MUSIC   %{F" THEME_B6 "}" WORKSPACE_6 "%{F-} CHAT   %{F" THEME_YELLOW "}" WORKSPACE_7 " GAMES%{F-}"))))
 
 ;; Hook for Polybar to update workspaces
