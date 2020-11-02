347fa33907fe89c887a227818857e46fe4ae1a0c
Demis Balbach <db@minikn.xyz>
2020-11-02 23:15:18 +0100

    Add lsp-treemacs config and toggle function

--- .config/doom/userconfig/treemacs.el
+++ .config/doom/userconfig/treemacs.el
@@ -1,5 +1,6 @@
 ;;; ../../.local/share/git/dotArch/.config/doom/userconfig/treemacs.el -*- lexical-binding: t; -*-
 
+;; treemacs
 (after! treemacs
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
@@ -9,5 +10,23 @@
         treemacs-silent-refresh t
         treemacs-recenter-after-file-follow 'always))
 
-;; (after! lsp-treemacs
-;;   (lsp-treemacs-sync-mode 1))
+;; lsp-treemacs
+(with-eval-after-load 'lsp-treemacs
+  (lsp-treemacs-sync-mode 1)
+  (setq lsp-treemacs-symbols-position-params
+        `((side . right)
+          (slot . 1)
+          (window-width . ,treemacs-width))))
+
+;; Toggle lsp-treemacs
+(defun db/lsp-treemacs-symbols-toggle ()
+  "Toggle the lsp-treemacs-symbols buffer."
+  (interactive)
+  (if (get-buffer "*LSP Symbols List*")
+      (kill-buffer "*LSP Symbols List*")
+    (progn (lsp-treemacs-symbols)
+           (other-window -1))))
+
+;; bind lsp-treemacs toggle
+(map! :leader
+      :desc "Toggle Symbols" "c S" #'db/lsp-treemacs-symbols-toggle)
