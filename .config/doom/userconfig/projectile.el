1a01167abe5ab97ece53e33700c3b9f046795144
Demis Balbach <db@minikn.xyz>
2020-11-02 23:16:39 +0100

    Add projectile config

---
+++ .config/doom/userconfig/projectile.el
@@ -0,0 +1,4 @@
+;;; ../../.local/share/git/dotArch/.config/doom/userconfig/projectile.el -*- lexical-binding: t; -*-
+
+(after! projectile
+  (setq projectile-track-known-projects-automatically nil))
