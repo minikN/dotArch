01dfab5db2e5719864d7b5b379d02b9aee234ff3
Demis Balbach <db@minikn.xyz>
2020-11-16 21:00:23 +0100

    Add trailing slash

--- .config/doom/userconfig/magit.el
+++ .config/doom/userconfig/magit.el
@@ -1,3 +1,3 @@
 ;;; ../../.local/share/git/dotArch/.config/doom/userconfig/magit.el -*- lexical-binding: t; -*-
 
-(setq magit-clone-default-directory (getenv "GITDIR"))
+(setq magit-clone-default-directory (concat (getenv "GITDIR") "/"))
