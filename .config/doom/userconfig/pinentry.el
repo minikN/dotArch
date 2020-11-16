82d9fd2420d45cd919f33aeff4ea89c439f6a433
Demis Balbach <db@minikn.xyz>
2020-11-16 22:12:15 +0100

    Add function to reload the GPG agent

--- .config/doom/userconfig/pinentry.el
+++ .config/doom/userconfig/pinentry.el
@@ -5,3 +5,14 @@
 
 ;; Start GPG agent with SSH support
 (shell-command "gpg-connect-agent /bye")
+
+;; Reload the GPG agent if pinentry hangs
+(defun db/reload-gpg-agent ()
+  "Reload the GPG agent."
+  (interactive)
+  (shell-command "gpgconf --kill gpg-agent")
+  (message "Reloaded the GPG agent"))
+
+;; Add keybinding to reload GPG agent
+(map! :leader
+      :desc "Reload GPG agent" "g a" #'db/reload-gpg-agent)
